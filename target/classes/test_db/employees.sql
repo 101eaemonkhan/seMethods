1ca355d28fcbf5924b84d0483f058f258bd3e803
Emon Ahmed <40646329@live.napier.ac.uk>
2023-02-07 21:40:33 +0600

    update our Actions file to use the Docker compose file

--- target/classes/test_db/employees.sql
+++ target/classes/test_db/employees.sql
@@ -1,26 +1,26 @@
---  Sample employee database
+--  Sample employee database 
 --  See changelog table for details
 --  Copyright (C) 2007,2008, MySQL AB
---
+--  
 --  Original data created by Fusheng Wang and Carlo Zaniolo
 --  http://www.cs.aau.dk/TimeCenter/software.htm
 --  http://www.cs.aau.dk/TimeCenter/Data/employeeTemporalDataSet.zip
---
---  Current schema by Giuseppe Maxia
+-- 
+--  Current schema by Giuseppe Maxia 
 --  Data conversion from XML to relational by Patrick Crews
---
--- This work is licensed under the
--- Creative Commons Attribution-Share Alike 3.0 Unported License.
--- To view a copy of this license, visit
--- http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
--- Creative Commons, 171 Second Street, Suite 300, San Francisco,
+-- 
+-- This work is licensed under the 
+-- Creative Commons Attribution-Share Alike 3.0 Unported License. 
+-- To view a copy of this license, visit 
+-- http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to 
+-- Creative Commons, 171 Second Street, Suite 300, San Francisco, 
 -- California, 94105, USA.
---
+-- 
 --  DISCLAIMER
 --  To the best of our knowledge, this data is fabricated, and
---  it does not correspond to real people.
+--  it does not correspond to real people. 
 --  Any similarity to existing people is purely coincidental.
---
+-- 
 
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE IF NOT EXISTS employees;
@@ -43,7 +43,7 @@ CREATE TABLE employees (
     birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,    
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
@@ -63,7 +63,7 @@ CREATE TABLE dept_manager (
    FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
    PRIMARY KEY (emp_no,dept_no)
-);
+); 
 
 CREATE TABLE dept_emp (
     emp_no      INT             NOT NULL,
@@ -82,7 +82,8 @@ CREATE TABLE titles (
     to_date     DATE,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,title, from_date)
-);
+) 
+; 
 
 CREATE TABLE salaries (
     emp_no      INT             NOT NULL,
@@ -91,7 +92,8 @@ CREATE TABLE salaries (
     to_date     DATE            NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no, from_date)
-);
+) 
+; 
 
 CREATE OR REPLACE VIEW dept_emp_latest_date AS
     SELECT emp_no, MAX(from_date) AS from_date, MAX(to_date) AS to_date
@@ -108,7 +110,7 @@ CREATE OR REPLACE VIEW current_dept_emp AS
 flush /*!50503 binary */ logs;
 
 SELECT 'LOADING departments' as 'INFO';
-source load_departments.dump;
+source load_departments.dump ;
 SELECT 'LOADING employees' as 'INFO';
 source load_employees.dump ;
 SELECT 'LOADING dept_emp' as 'INFO';
