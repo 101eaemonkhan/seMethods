4a5f034109a72547407292bc8d13b02bd55d90e5
Emon Ahmed <40646329@live.napier.ac.uk>
2023-02-09 23:20:43 +0600

    Display Employee Information

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -76,20 +76,37 @@ public class App
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
+
+//            String strSelect =
+//                    "SELECT emp_no, first_name, last_name "
+//                            + "FROM employees "
+//                            + "WHERE emp_no = " + ID;
+
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT employees.emp_no, first_name, last_name, titles.title as 'title', salaries.salary as 'salary', departments.dept_name as 'dept_name' "
+                            + ", (SELECT CONCAT(first_name ,' ',last_name)  FROM employees WHERE employees.emp_no = dept_manager.emp_no) as 'manager' "
+                            + "FROM employees inner join titles on employees.emp_no = titles.emp_no "
+                            + "INNER JOIN salaries on employees.emp_no = salaries.emp_no "
+                            + "INNER JOIN dept_emp on employees.emp_no = dept_emp.emp_no "
+                            + "INNER JOIN departments on dept_emp.dept_no = departments.dept_no "
+                            + "INNER JOIN dept_manager on dept_emp.dept_no = dept_manager.dept_no "
+                            + "WHERE employees.emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
             if (rset.next())
             {
+                System.out.println(rset);
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
+
                 return emp;
             }
             else
