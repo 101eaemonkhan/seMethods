2e313685a1b20daebdeaf65f0d27510625a23d00
Emon Ahmed <40646329@live.napier.ac.uk>
2023-04-20 16:44:59 +0600

    AppIntegrationTest

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -16,7 +16,7 @@ public class AppIntegrationTest
     static void init()
     {
         app = new App();
-        app.connect("localhost:33060", 30000);
+        app.connect("localhost:8080", 80000);
 
     }
 
