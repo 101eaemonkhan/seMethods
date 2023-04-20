f0b8c4fe50fa3feea5f2d99fc01b6bb9008495b8
Emon Ahmed <40646329@live.napier.ac.uk>
2023-04-20 17:32:23 +0600

    Add New Employee

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -243,6 +243,23 @@ public class App {
             );
         }
     }
+    public void addEmployee(Employee emp)
+    {
+        try
+        {
+            Statement stmt = con.createStatement();
+            String strUpdate =
+                    "INSERT INTO employees (emp_no, first_name, last_name, birth_date, gender, hire_date) " +
+                            "VALUES (" + emp.emp_no + ", '" + emp.first_name + "', '" + emp.last_name + "', " +
+                            "'9999-01-01', 'M', '9999-01-01')";
+            stmt.execute(strUpdate);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to add employee");
+        }
+    }
 
     public ArrayList<Employee> getSalariesByTitle(String title) {
         try {
--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -28,4 +28,25 @@ public class AppIntegrationTest
         assertEquals(emp.first_name, "Ronghao ");
         assertEquals(emp.last_name, "Garigliano ");
     }
+    @Test
+    void testsalary()
+    {
+        Employee emp = app.getEmployee(255530);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.salary, "20000 ");
+        assertEquals(emp.dept, "engineer ");
+    }
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
